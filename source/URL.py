aa0f041bd20541d9f8b35fa763a445d7f4d9b597
SHIELDXIE <37254173+SHIELDXIE@users.noreply.github.com>
2022-04-02 23:17:06 +0800

    run with v1.2

---
+++ source/URL.py
@@ -0,0 +1,10 @@
+#填写账号
+account = '000000000000' 
+#填写密码
+password = '00000000'    
+#get参数
+argumentsA = 'http://183.6.94.28/drcom/login?callback=dr1003&DDDDD='
+argumentsB = '%40GZARTS.GZ&upass='
+argumentsC = '&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&v=0000'
+
+get_url = (argumentsA + account + argumentsB + password + argumentsC)
